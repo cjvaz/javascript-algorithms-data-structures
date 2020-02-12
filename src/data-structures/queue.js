ec03f9c7402d86a95333a38c3887bcfb9905a1c8
Carlos Vaz <carlos-vaz@procergs.rs.gov.br>
2020-02-12 11:42:08 -0300

    initial Queue test and methods isEmpty enqueue, dequeue, peek and size

---
+++ src/data-structures/queue.js
@@ -0,0 +1,40 @@
+class Queue {
+
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  enqueue(ele) {
+    this.items[this.count] = ele;
+    this.count++;
+  }
+
+  dequeue() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    const ele = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return ele;
+  }
+
+  peek() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.lowestCount];
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+}
+
+module.exports = Queue;
