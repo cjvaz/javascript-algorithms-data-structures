041ae35e695fab86e811ab0ad4cdcf972b7121c2
Carlos Vaz <carlos-vaz@procergs.rs.gov.br>
2020-03-16 14:40:43 -0300

    removed t.js

--- src/t.js
+++
@@ -1,8 +0,0 @@
-const MinHeap = require('./data-structures/heap');
-const heap = new MinHeap();
-heap.insert(2);
-heap.insert(3);
-heap.insert(4);
-heap.insert(5);
-heap.insert(1);
-console.log(heap.getAsArray());
